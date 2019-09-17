ed0ce6bdec077e44db9a56f2f7dbab0d09e3d9cc
Jeff Schulz <jeffschulz7@gmail.com>
2019-09-17 09:50:07 -0500

    End week 2 - parsing instruction types

--- Lab2/src/mips-assembler.c
+++ Lab2/src/mips-assembler.c
@@ -19,11 +19,12 @@ void load_program(char* prog_file) {
 
 	/* Read in the program. */
 	while((read = getline(&word,&len,fp)) != EOF ) {
-		printf("From file read: %s",word);
+		//printf("From file read: %s",word);
 		handle_instruction(word);
 	}
 	fclose(fp);
 }
+
 
 
 /**************************************************************/
@@ -31,39 +32,76 @@ void load_program(char* prog_file) {
 /**************************************************************/
 void handle_instruction(char* instr) {                  
 	char* opcode = strtok(instr," ");
-	char** params;
-	printf("opcode: %s\n",opcode);
+	char params[4][6];
+	//params = malloc(4*5*sizeof(char));
+	//printf("opcode: %s\n",opcode);
 	int dim;
 
 	opcode = to_lower(opcode);
 
 	if(opcode == "add") {
 		dim = 3;
-		params = parse_params(dim);
+		//strcpy(params,parse_params(dim));
+		printf("%s %s %s",params[0],params[1],params[2]);
 		//parse_registers(2)
 		//parse_immediate(1)
 		//create_i_type()
 
 	} else if(opcode == "addi") {
-
+		;
+	} else if(!strncmp(opcode,"addiu",5)) {
+		dim = 3;
+		parse_params(params,dim);
+		printf("\n%s %s %s %s",opcode,params[0],params[1],params[2]);
+		parse_registers('i',params);
 	}
 
+}
 
 
+void parse_registers(char t,char params[4][6]) {
+	switch(t) {
+		case 'i':
+			i_type_data data_i;
+			data_i.rt 		 = get_int(params[0]);
+			data_i.rs 		 = get_int(params[1]);
+			data_i.immediate = get_int(params[2]);
+			break;
+		case 'r':
+			r_type_data data_r;
+			data_r.rt 		 = get_int(params[0]);
+			data_r.rs 		 = get_int(params[1]);
+			data_r.rd
+			data_r.shamt 	 = get_int(params[2]);
+			break;
+		case 'j':
+			j_type_data data_j;
+			data_j.target	 = get_int(params[0]);
+			break;
+	}
+}
 
+
+int get_int(char* str) {
+	char temp[strlen(str)];
+	int i=1;
+	while(isdigit(str[i]) != 0) {
+		temp[i-1] = str[i];
+		i++;
+	}
+	return strtol(temp,NULL,0);
 }
 
 
 /**************************************************************/
 /**/
 /**************************************************************/
-char** parse_params(int dim) {
+void parse_params(char params[4][6],int dim) {
 	int i;
-	char** params;
+	//char params[4][6];
 	for(i=0; i<dim; i++) {
-		params[i] = strtok(NULL," ");
+		strcpy(params[i],strtok(NULL," "));
 	}
-	return params;
 }
 
 
@@ -81,9 +119,8 @@ char* to_lower(char* str) {
 }
 
 
-
 /***************************************************************/
-/* main   */                                                                                                                                */
+/*main*/
 /***************************************************************/
 int main(int argc, char *argv[]) {                              
 	printf("\n**************************\n");
--- Lab2/src/mips-assembler.h
+++ Lab2/src/mips-assembler.h
@@ -11,4 +11,23 @@
 void load_program(char* prog_file);
 void handle_instruction(char* instr);
 char* to_lower(char* str);
+void parse_params(char params[4][6],int dim);
+void parse_registers(char t,char params[4][6]);
+int get_int(char* str);
 
+typedef struct R_Type_Instruction {
+	uint32_t rs;
+	uint32_t rt;
+	uint32_t rd;
+	uint32_t shamt;
+} r_type_data;
+
+typedef struct I_Type_Instruction {
+	uint32_t rs;
+	uint32_t rt;
+	uint32_t immediate;
+} i_type_data;
+
+typedef struct J_Type_Instruction {
+	uint32_t target;
+} j_type_data;
